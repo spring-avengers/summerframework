aa8038958d0c374d34df9a6a231afffb2f690a5b
shiming <ming@binance.com>
2019-06-17 15:52:21 +0800

    fix dependencies

--- install-all.sh
+++ install-all.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 
-cd summerframework-common        && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-configcenter  && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-mybatis       && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-webapi        && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-eureka        && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-openfeign     && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-monitor       && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-common        && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-configcenter  && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-mybatis       && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-webapi        && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-eureka        && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-openfeign     && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-redis         && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-rabbit        && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
+cd summerframework-monitor       && mvn install -U -DskipTests -Dmaven.javadoc.skip=true && cd ..
